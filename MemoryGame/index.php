77f0db55945b5e939e39fc4bd0b64615e292b8be
PcKiLl3r <Zorko.Grega@gmail.com>
2021-12-07 11:57:38 +0100

    MemoryGame

---
+++ MemoryGame/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="container">
+
+<div class="memory-cards">
+
+<div class="memory-card">
+    <div class="memory-card-front">
+        ?
+    </div>
+    <div class="memory-card-left">
+    </div>
+    <div class="memory-card-bot">
+    </div>
+    <div class="memory-card-right">
+    </div>
+    <div class="memory-card-back">
+    </div>
+    <div class="memory-card-top">
+    </div>
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+<div class="memory-card">
+</div>
+
+</div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+</html>
