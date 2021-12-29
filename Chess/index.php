d2a3b97a297a1d5a7529bb08311b0b0a76d2ef4d
PcKiLl3r <Zorko.Grega@gmail.com>
2021-12-29 14:24:41 +0100

    chess

---
+++ Chess/index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./style.css">
+
+</head>
+
+<body>
+    <div class="scene">
+        <div class="chessboard">
+            <div class="fieldMarksVertical">
+                <div class="mark mark1">1</div>
+                <div class="mark mark2">2</div>
+                <div class="mark mark3">3</div>
+                <div class="mark mark4">4</div>
+                <div class="mark mark5">5</div>
+                <div class="mark mark6">6</div>
+                <div class="mark mark7">7</div>
+                <div class="mark mark8">8</div>
+            </div>
+            <div class="fieldMarksHorizontal">
+                <div class="bottomLeftBox"></div>
+                <div class="bottomRightBox"></div>
+                <div class="mark markA">A</div>
+                <div class="mark markB">B</div>
+                <div class="mark markC">C</div>
+                <div class="mark markD">D</div>
+                <div class="mark markE">E</div>
+                <div class="mark markF">F</div>
+                <div class="mark markG">G</div>
+                <div class="mark markH">H</div>
+            </div>
+            <div class="fieldMarksVertical2">
+                <div class="mark mark1">1</div>
+                <div class="mark mark2">2</div>
+                <div class="mark mark3">3</div>
+                <div class="mark mark4">4</div>
+                <div class="mark mark5">5</div>
+                <div class="mark mark6">6</div>
+                <div class="mark mark7">7</div>
+                <div class="mark mark8">8</div>
+            </div>
+            <div class="fieldMarksHorizontal2">
+                <div class="bottomLeftBox bg-black"></div>
+                <div class="bottomRightBox bg-black"></div>
+                <div class="mark markA">A</div>
+                <div class="mark markB">B</div>
+                <div class="mark markC">C</div>
+                <div class="mark markD">D</div>
+                <div class="mark markE">E</div>
+                <div class="mark markF">F</div>
+                <div class="mark markG">G</div>
+                <div class="mark markH">H</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+            <div class="row">
+                <div class="field">&nbsp;
+                    <div class="figure figure-peasant"></div>
+                </div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+                <div class="field">&nbsp;</div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
