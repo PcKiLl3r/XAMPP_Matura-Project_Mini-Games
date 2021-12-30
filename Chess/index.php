71e8274cc99de270af62314c0ba23c897278e4f3
PcKiLl3r <Zorko.Grega@gmail.com>
2021-12-30 20:53:10 +0100

    chess path highlighting

--- Chess/index.php
+++ Chess/index.php
@@ -9,7 +9,7 @@
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
         integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"> -->
 
-        <script src="https://kit.fontawesome.com/6e0b87b468.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6e0b87b468.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="./style.css">
 
@@ -62,8 +62,8 @@
                 <div class="mark markG">G</div>
                 <div class="mark markH">H</div>
             </div>
-            <div class="row">
-                <div tabindex="64" class="field">&nbsp;
+            <div id="row1" class="row">
+                <div tabindex="1" class="field">&nbsp;
                     <div class="figure figure-black">
                         <div class="chessCard">
                             <div class="card-top bg-rook-white">
@@ -76,7 +76,7 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="63" class="field">&nbsp;
+                <div tabindex="2" class="field">&nbsp;
                     <div class="figure figure-black">
                         <div class="chessCard">
                             <div class="card-top bg-knight-white"></div>
@@ -88,7 +88,7 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="62" class="field">&nbsp;
+                <div tabindex="3" class="field">&nbsp;
                     <div class="figure figure-black">
                         <div class="chessCard">
                             <div class="card-top bg-bishop-white"></div>
@@ -100,19 +100,7 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="61" class="field">&nbsp;
-                    <div class="figure figure-black">
-                        <div class="chessCard">
-                            <div class="card-top bg-king-white"></div>
-                            <div class="card-bot"></div>
-                            <div class="card-left"></div>
-                            <div class="card-right"></div>
-                            <div class="card-front"></div>
-                            <div class="card-back"></div>
-                        </div>
-                    </div>
-                </div>
-                <div tabindex="60" class="field">&nbsp;
+                <div tabindex="4" class="field">&nbsp;
                     <div class="figure figure-black">
                         <div class="chessCard">
                             <div class="card-top bg-queen-white"></div>
@@ -124,15 +112,10 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="59" class="field">&nbsp;</div>
-                <div tabindex="58" class="field">&nbsp;</div>
-                <div tabindex="57" class="field">&nbsp;</div>
-            </div>
-            <div class="row">
-                <div tabindex="56" class="field">&nbsp;
+                <div tabindex="5" class="field">&nbsp;
                     <div class="figure figure-black">
                         <div class="chessCard">
-                            <div class="card-top bg-pawn-white"></div>
+                            <div class="card-top bg-king-white"></div>
                             <div class="card-bot"></div>
                             <div class="card-left"></div>
                             <div class="card-right"></div>
@@ -141,91 +124,108 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="55" class="field">&nbsp;</div>
-                <div tabindex="54" class="field">&nbsp;</div>
-                <div tabindex="53" class="field">&nbsp;</div>
-
-                <div tabindex="52" class="field">&nbsp;</div>
-                <div tabindex="51" class="field">&nbsp;</div>
-                <div tabindex="50" class="field">&nbsp;</div>
-                <div tabindex="49" class="field">&nbsp;
-                    <div class="figure figure-black">
-                        <div class="chessCard">
-                            <div class="card-top bg-pawn-white"></div>
-                            <div class="card-bot"></div>
-                            <div class="card-left"></div>
-                            <div class="card-right"></div>
-                            <div class="card-front"></div>
-                            <div class="card-back"></div>
-                        </div>
-                    </div>
-                </div>
+                <div tabindex="6" class="field">&nbsp;</div>
+                <div tabindex="7" class="field">&nbsp;</div>
+                <div tabindex="8" class="field">&nbsp;</div>
             </div>
-            <div class="row">
-                <div tabindex="48" class="field">&nbsp;</div>
-                <div tabindex="47" class="field">&nbsp;</div>
-                <div tabindex="46" class="field">&nbsp;</div>
-                <div tabindex="45" class="field">&nbsp;</div>
-
-                <div tabindex="44" class="field">&nbsp;</div>
-                <div tabindex="43" class="field">&nbsp;</div>
-                <div tabindex="42" class="field">&nbsp;</div>
-                <div tabindex="41" class="field">&nbsp;</div>
-            </div>
-            <div class="row">
-                <div tabindex="40" class="field">&nbsp;</div>
-                <div tabindex="39" class="field">&nbsp;</div>
-                <div tabindex="38" class="field">&nbsp;</div>
-                <div tabindex="37" class="field">&nbsp;</div>
-
-                <div tabindex="36" class="field">&nbsp;</div>
-                <div tabindex="35" class="field">&nbsp;</div>
-                <div tabindex="34" class="field">&nbsp;</div>
-                <div tabindex="33" class="field">&nbsp;</div>
-            </div>
-            <div class="row">
-                <div tabindex="32" class="field">&nbsp;</div>
-                <div tabindex="31" class="field">&nbsp;</div>
-                <div tabindex="30" class="field">&nbsp;</div>
-                <div tabindex="29" class="field">&nbsp;</div>
-
-                <div tabindex="28" class="field">&nbsp;</div>
-                <div tabindex="27" class="field">&nbsp;</div>
-                <div tabindex="26" class="field">&nbsp;</div>
-                <div tabindex="25" class="field">&nbsp;</div>
-            </div>
-            <div class="row">
-                <div tabindex="24" class="field">&nbsp;</div>
-                <div tabindex="23" class="field">&nbsp;</div>
-                <div tabindex="22" class="field">&nbsp;</div>
-                <div tabindex="21" class="field">&nbsp;</div>
-
-                <div tabindex="20" class="field">&nbsp;</div>
-                <div tabindex="19" class="field">&nbsp;</div>
-                <div tabindex="18" class="field">&nbsp;</div>
-                <div tabindex="17" class="field">&nbsp;</div>
-            </div>
-            <div class="row">
-                <div tabindex="16" class="field">&nbsp;
-                    <div class="figure figure-white">
-                        <div class="chessCard">
-                            <div class="card-top bg-pawn"></div>
-                            <div class="card-bot"></div>
-                            <div class="card-left"></div>
-                            <div class="card-right"></div>
-                            <div class="card-front"></div>
-                            <div class="card-back"></div>
-                        </div>
-                    </div>
-                </div>
-                <div tabindex="15" class="field">&nbsp;</div>
-                <div tabindex="14" class="field">&nbsp;</div>
-                <div tabindex="13" class="field">&nbsp;</div>
-
-                <div tabindex="12" class="field">&nbsp;</div>
-                <div tabindex="11" class="field">&nbsp;</div>
-                <div tabindex="10" class="field">&nbsp;</div>
+            <div id="row2" class="row">
                 <div tabindex="9" class="field">&nbsp;
+                    <div class="figure figure-black">
+                        <div class="chessCard">
+                            <div class="card-top bg-pawn-white"></div>
+                            <div class="card-bot"></div>
+                            <div class="card-left"></div>
+                            <div class="card-right"></div>
+                            <div class="card-front"></div>
+                            <div class="card-back"></div>
+                        </div>
+                    </div>
+                </div>
+                <div tabindex="10" class="field">&nbsp;</div>
+                <div tabindex="11" class="field">&nbsp;</div>
+                <div tabindex="12" class="field">&nbsp;</div>
+
+                <div tabindex="13" class="field">&nbsp;</div>
+                <div tabindex="14" class="field">&nbsp;</div>
+                <div tabindex="15" class="field">&nbsp;</div>
+                <div tabindex="16" class="field">&nbsp;
+                    <div class="figure figure-black">
+                        <div class="chessCard">
+                            <div class="card-top bg-pawn-white"></div>
+                            <div class="card-bot"></div>
+                            <div class="card-left"></div>
+                            <div class="card-right"></div>
+                            <div class="card-front"></div>
+                            <div class="card-back"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="row3" class="row">
+                <div tabindex="17" class="field">&nbsp;</div>
+                <div tabindex="18" class="field">&nbsp;</div>
+                <div tabindex="19" class="field">&nbsp;</div>
+                <div tabindex="20" class="field">&nbsp;</div>
+
+                <div tabindex="21" class="field">&nbsp;</div>
+                <div tabindex="22" class="field">&nbsp;</div>
+                <div tabindex="23" class="field">&nbsp;</div>
+                <div tabindex="24" class="field">&nbsp;</div>
+            </div>
+            <div id="row4" class="row">
+                <div tabindex="25" class="field">&nbsp;</div>
+                <div tabindex="26" class="field">&nbsp;</div>
+                <div tabindex="27" class="field">&nbsp;</div>
+                <div tabindex="28" class="field">&nbsp;</div>
+
+                <div tabindex="29" class="field">&nbsp;</div>
+                <div tabindex="30" class="field">&nbsp;</div>
+                <div tabindex="31" class="field">&nbsp;</div>
+                <div tabindex="32" class="field">&nbsp;</div>
+            </div>
+            <div id="row5" class="row">
+                <div tabindex="33" class="field">&nbsp;</div>
+                <div tabindex="34" class="field">&nbsp;</div>
+                <div tabindex="35" class="field">&nbsp;</div>
+                <div tabindex="36" class="field">&nbsp;</div>
+
+                <div tabindex="37" class="field">&nbsp;</div>
+                <div tabindex="38" class="field">&nbsp;</div>
+                <div tabindex="39" class="field">&nbsp;</div>
+                <div tabindex="40" class="field">&nbsp;</div>
+            </div>
+            <div id="row6" class="row">
+                <div tabindex="41" class="field">&nbsp;</div>
+                <div tabindex="42" class="field">&nbsp;</div>
+                <div tabindex="43" class="field">&nbsp;</div>
+                <div tabindex="44" class="field">&nbsp;</div>
+
+                <div tabindex="45" class="field">&nbsp;</div>
+                <div tabindex="46" class="field">&nbsp;</div>
+                <div tabindex="47" class="field">&nbsp;</div>
+                <div tabindex="48" class="field">&nbsp;</div>
+            </div>
+            <div id="row7" class="row">
+                <div tabindex="49" class="field">&nbsp;
+                    <div class="figure figure-white">
+                        <div class="chessCard">
+                            <div class="card-top bg-pawn"></div>
+                            <div class="card-bot"></div>
+                            <div class="card-left"></div>
+                            <div class="card-right"></div>
+                            <div class="card-front"></div>
+                            <div class="card-back"></div>
+                        </div>
+                    </div>
+                </div>
+                <div tabindex="50" class="field">&nbsp;</div>
+                <div tabindex="51" class="field">&nbsp;</div>
+                <div tabindex="52" class="field">&nbsp;</div>
+
+                <div tabindex="53" class="field">&nbsp;</div>
+                <div tabindex="54" class="field">&nbsp;</div>
+                <div tabindex="55" class="field">&nbsp;</div>
+                <div tabindex="56" class="field">&nbsp;
                     <div class="figure figure-white">
                         <div class="chessCard">
                             <div class="card-top bg-pawn"></div>
@@ -238,8 +238,8 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div tabindex="8" class="field">&nbsp;
+            <div id="row8" class="row">
+                <div tabindex="57" class="field">&nbsp;
                     <div class="figure figure-white">
                         <div class="chessCard">
                             <div class="card-top bg-rook">
@@ -252,7 +252,7 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="7" class="field">&nbsp;
+                <div tabindex="58" class="field">&nbsp;
                     <div class="figure figure-white">
                         <div class="chessCard">
                             <div class="card-top bg-knight"></div>
@@ -264,7 +264,7 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="6" class="field">&nbsp;
+                <div tabindex="59" class="field">&nbsp;
                     <div class="figure figure-white">
                         <div class="chessCard">
                             <div class="card-top bg-bishop"></div>
@@ -276,20 +276,7 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="5" class="field">&nbsp;
-                    <div class="figure figure-white">
-                        <div class="chessCard">
-                            <div class="card-top bg-king"></div>
-                            <div class="card-bot"></div>
-                            <div class="card-left"></div>
-                            <div class="card-right"></div>
-                            <div class="card-front"></div>
-                            <div class="card-back"></div>
-                        </div>
-                    </div>
-                </div>
-
-                <div tabindex="4" class="field">&nbsp;
+                <div tabindex="60" class="field">&nbsp;
                     <div class="figure figure-white">
                         <div class="chessCard">
                             <div class="card-top bg-queen"></div>
@@ -301,9 +288,22 @@
                         </div>
                     </div>
                 </div>
-                <div tabindex="3" class="field">&nbsp;</div>
-                <div tabindex="2" class="field">&nbsp;</div>
-                <div tabindex="1" class="field">&nbsp;</div>
+
+                <div tabindex="61" class="field">&nbsp;
+                    <div class="figure figure-white">
+                        <div class="chessCard">
+                            <div class="card-top bg-king"></div>
+                            <div class="card-bot"></div>
+                            <div class="card-left"></div>
+                            <div class="card-right"></div>
+                            <div class="card-front"></div>
+                            <div class="card-back"></div>
+                        </div>
+                    </div>
+                </div>
+                <div tabindex="62" class="field">&nbsp;</div>
+                <div tabindex="63" class="field">&nbsp;</div>
+                <div tabindex="64" class="field">&nbsp;</div>
             </div>
         </div>
     </div>
--- Chess/selector.js
+++ Chess/selector.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let blackFigure = document.createElement('div');
     blackFigure.classList.add('figure','figure-black');
 
-    let whitePawnText = `<div class="chessCard">
+let whitePawnText = `<div class="chessCard">
     <div class="card-top bg-pawn-white"></div>
     <div class="card-bot"></div>
     <div class="card-left"></div>
@@ -60,7 +60,7 @@ let whiteQueenText = `<div class="chessCard">
     <div class="card-back"></div>
 </div>`;
 
-    let blackPawnText = `<div class="chessCard">
+let blackPawnText = `<div class="chessCard">
     <div class="card-top bg-pawn"></div>
     <div class="card-bot"></div>
     <div class="card-left"></div>
@@ -116,6 +116,8 @@ let blackQueenText = `<div class="chessCard">
 
 let fields = document.querySelectorAll('.field');
 
+let highlightedFields = [];
+
     function resetBoard(){
         console.log("Board Reset!");
         let fields = document.querySelectorAll('.field');
@@ -141,13 +143,13 @@ let fields = document.querySelectorAll('.field');
                 figure.innerHTML = whiteBishopText;
                 fields[i].appendChild(figure);
             }
-            if(i == 4)
+            if(i == 3)
             {
                 let figure = blackFigure.cloneNode();
                 figure.innerHTML = whiteQueenText;
                 fields[i].appendChild(figure);
             }
-            if(i == 3)
+            if(i == 4)
             {
                 let figure = blackFigure.cloneNode();
                 figure.innerHTML = whiteKingText;
@@ -173,16 +175,24 @@ let fields = document.querySelectorAll('.field');
                 figure.innerHTML = blackBishopText;
                 fields[i].appendChild(figure);
             }
-            if(i == 60){
+            if(i == 59){
                 let figure = whiteFigure.cloneNode();
                 figure.innerHTML = blackQueenText;
                 fields[i].appendChild(figure);
             }
-            if(i == 59){
+            if(i == 60){
                 let figure = whiteFigure.cloneNode();
                 figure.innerHTML = blackKingText;
                 fields[i].appendChild(figure);
             }
+        }
+    }
+
+    function wipeBoard(){
+        console.log("Board Reset!");
+        let fields = document.querySelectorAll('.field');
+        for(let i = 0; i < fields.length; i++){
+            fields[i].innerHTML = i;
         }
     }
 
@@ -192,13 +202,254 @@ let fields = document.querySelectorAll('.field');
                 resetBoard();
             });
         }
+        if(index == fields.length - 2){
+            fields[index].addEventListener('click', () => {
+                wipeBoard();
+            });
+        }
     }
 
+    let isOdd = true;
+
     fields.forEach(field => {
+
+        if(!isOdd){
+            field.classList.add('field-white');
+            isOdd = !isOdd;
+        } else {
+            field.classList.add('field-black');
+            isOdd = !isOdd;
+        }
+
         field.addEventListener('click', (e) => {
-            e.target.focus();
+
+            let figureType;
+
+            let fieldNumber;
+
+            clearHighlightedFields();
+
+            if(e.target.classList.contains('field')){
+                if(e.target.children[0] == undefined){
+                    console.log("Empty field clicked!");
+                    return;
+                } else {
+                    console.log(e.target);
+                    figureType = e.target.children[0].children[0].children[0].classList;
+                    figureType = figureType[1];
+                    fieldNumber = parseInt(e.target.tabIndex);
+                    console.log(figureType);
+                    console.log(fieldNumber);
+                }
+            } else if(e.target.classList.contains('figure')){
+
+                console.log(e.target);
+
+                console.log(e.target.children[0].children[0]);
+                figureType = e.target.children[0].children[0].classList;
+                figureType = figureType[1];
+
+                /* let figureColor = e.target.classList;
+                    figureColor = figureColor[1]; */
+    
+                    // logs bg-queen
+                    console.log(figureType);
+
+                    fieldNumber = parseInt(e.target.parentNode.tabIndex);
+                console.log(fieldNumber);
+                    /* console.log(figureColor); */
+                
+            } else if(e.target.classList.contains('chessCard')){
+                
+                console.log(e.target);
+                figureType = e.target.children[0].classList;
+                figureType = figureType[1];
+
+                /* let figureColor = e.target.parentNode.classList;
+                    figureColor = figureColor[1]; */
+    
+                    // logs bg-queen
+                    console.log(figureType);
+
+                    fieldNumber = parseInt(e.target.parentNode.parentNode.tabIndex);
+                console.log(fieldNumber);
+                    /* console.log(figureColor); */
+
+            } else if(e.target.classList.contains('card-top')){
+
+                console.log(e.target);
+                figureType = e.target.classList;
+                figureType = figureType[1];
+
+                /* let figureColor = e.target.parentNode.parentNode.classList;
+                    figureColor = figureColor[1]; */
+    
+                    // logs bg-queen
+                    console.log(figureType);
+                    fieldNumber = parseInt(e.target.parentNode.parentNode.parentNode.tabIndex);
+                console.log(fieldNumber);
+                    /* console.log(figureColor); */
+
+            } else if(e.target.classList.contains('card-front') || e.target.classList.contains('card-back') || e.target.classList.contains('card-left') || e.target.classList.contains('card-right') || e.target.classList.contains('card-bot')){
+
+                console.log(e.target);
+                figureType = e.target.parentNode.children[0].classList;
+                figureType = figureType[1];
+
+                /* let figureColor = e.target.parentNode.parentNode.classList;
+                    figureColor = figureColor[1]; */
+    
+                    // logs bg-queen
+                    console.log(figureType);
+                    fieldNumber = parseInt(e.target.parentNode.parentNode.parentNode.tabIndex);
+                console.log(fieldNumber);
+                    /* console.log(figureColor); */
+
+            }
+
+            fieldNumber--;
+
+            // Switch figureType
+
+            switch (figureType) {
+                case 'bg-pawn':
+
+                    let fieldsAhead = getFieldsAhead(fields[fieldNumber], fieldNumber, 1)
+
+                    console.log(fieldNumber);
+
+                    if(fieldNumber >= 46 && fieldNumber <= 55){
+                            fieldsAhead = fieldsAhead.slice(0, 2);
+                        } else {
+                            fieldsAhead = fieldsAhead.slice(0, 1);
+                        }
+
+                    highlightFieldsAhead(fieldsAhead);
+                    
+                    break;
+
+                    case 'bg-pawn-white':
+
+
+
+                    break;
+            
+                default:
+                    break;
+            }
+
+
+
         })
     });
+
+    function clearHighlightedFields(){
+        for (let index = 0; index < highlightedFields.length; index++) {
+            if(fields[highlightedFields[index]].classList.contains('focusedFieldBlack')){
+                fields[highlightedFields[index]].classList.remove('focusedFieldBlack');
+            } else {
+                fields[highlightedFields[index]].classList.remove('focusedFieldWhite');
+            }
+            console.log("Clearing Highlighted Field: " + highlightedFields[index]);
+        }
+        highlightedFields = [];
+    }
+
+    function highlightFieldsAhead(_fields){
+        let isOdd = false;
+        if(fields[_fields[0]].classList.contains('field-white')){
+            isOdd = true;
+        }
+        for (let index = 0; index < _fields.length; index++) {
+            if(isOdd){
+                fields[_fields[index]].classList.add('focusedFieldBlack');
+            } else {
+                fields[_fields[index]].classList.add('focusedFieldWhite');
+            }
+            highlightedFields.push(_fields[index]);
+            isOdd = !isOdd;
+        }
+    }
+
+    function getFieldsBehind(isWhite){
+        
+    }
+
+    function getFieldsAhead(field, fieldNumber, isWhite){
+         let isOdd = field.parentNode.id;
+         let fieldsAhead = [];
+         isOdd = isOdd.slice(3, 4);
+         isOdd = parseInt(isOdd);
+         let row = isOdd;
+         /* isOdd = isOdd % 2;
+         console.log(isOdd); */
+            if(isWhite){
+                // isWhite num of field should first decrement by 1
+                for (let index = 0; index < row; index++) {
+
+                    if(index == 0){
+                        fieldsAhead[index] = fieldNumber - 8;
+                    } else {
+                        if(fieldsAhead[index - 1] - 8 < 0){
+                            break;
+                        }
+                        fieldsAhead[index] = fieldsAhead[index - 1] - 8;
+                        /* if(isOdd){
+                            if(fieldsAhead[index - 1] - 15 < 0){
+                                break;
+                            }
+                            fieldsAhead[index] = fieldsAhead[index - 1] - 15;
+                        } else {
+                            if(fieldsAhead[index - 1] - 1 < 0){
+                                break;
+                            }
+                            fieldsAhead[index] = fieldsAhead[index - 1] - 1;
+                        } */
+                    }
+                    /* isOdd = !isOdd; */
+
+                }
+                
+            } else {
+                // isWhite NOT TRUE num of field first should increment by 1
+                for (let index = 0; index < 8 - row; index++) {
+
+                    if(index == 0){
+                        fieldsAhead[index] = fieldNumber + 8;
+                    } else {
+                        if(fieldsAhead[index - 1] + 8 > 64){
+                            break;
+                        }
+                        fieldsAhead[index] = fieldsAhead[index - 1] + 8;
+                    }
+                    
+                }
+                fieldsAhead.reverse();
+            }
+         for (let index = 0; index < fieldsAhead.length; index++) {
+             
+            console.log("Can move on: " + fieldsAhead[index]);
+
+            return(fieldsAhead);
+             
+         }
+     }
+
+    function getFieldsLeft(isWhite){
+        
+    }
+
+    function getFieldsRight(isWhite){
+        
+    }
+
+    /* let figures = document.querySelectorAll('.figure');
+
+    figures.forEach(figure => {
+        figure.addEventListener('click', (e) => {
+            e.target.parentNode.click();
+        })
+    }); */
 
 
 
